e3ab70c8927c1719edc85530d9830682f9d0ca98
Andrey O <ozzy.ext@gmail.com>
2022-02-08 14:29:22 +0300

    Initial implementation

--- src/MyLab.ShellTask/Program.cs
+++ src/MyLab.ShellTask/Program.cs
@@ -1,15 +1,37 @@
+using MyLab.ShellTask;
+using MyLab.TaskApp;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddTaskLogic<ShellTaskLogic>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseAuthorization();
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
 
-app.MapControllers();
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+       new WeatherForecast
+       (
+           DateTime.Now.AddDays(index),
+           Random.Shared.Next(-20, 55),
+           summaries[Random.Shared.Next(summaries.Length)]
+       ))
+        .ToArray();
+    return forecast;
+});
 
 app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ src/MyLab.ShellTask/ShellHelper.cs
@@ -0,0 +1,68 @@
+﻿using System.Diagnostics;
+using MyLab.Log.Dsl;
+
+namespace MyLab.ShellTask;
+
+public static class ShellHelper
+{
+    public static Task<int> Bash(string cmd, IDslLogger? logger)
+    {
+        var source = new TaskCompletionSource<int>();
+        var escapedArgs = cmd.Replace("\"", "\\\"");
+        var process = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "bash",
+                Arguments = $"-c \"{escapedArgs}\"",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            },
+            EnableRaisingEvents = true
+        };
+        process.Exited += (sender, args) =>
+        {
+            if (logger != null)
+            {
+                var procError = process.StandardError.ReadToEnd();
+                var procOut = process.StandardOutput.ReadToEnd();
+
+                var logMsg = string.IsNullOrWhiteSpace(procError)
+                    ? logger.Error("Script execution error")
+                        .AndFactIs("stderr", procError)
+                    : logger.Action("Script execution completed");
+
+                logMsg
+                    .AndFactIs("stdout", procOut)
+                    .AndFactIs("exit-code", process.ExitCode)
+                    .Write();
+            }
+
+
+            if (process.ExitCode == 0)
+            {
+                source.SetResult(0);
+            }
+            else
+            {
+                source.SetException(new Exception($"Script execution failed with exit code `{process.ExitCode}`"));
+            }
+
+            process.Dispose();
+        };
+
+        try
+        {
+            process.Start();
+        }
+        catch (Exception e)
+        {
+            logger.Error("Script execution failed", e).Write();
+            source.SetException(e);
+        }
+
+        return source.Task;
+    }
+}
